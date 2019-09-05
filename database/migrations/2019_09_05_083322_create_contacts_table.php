0787b99deeab9e4203a5a01385792424e78cf475
Imtiaz Epu <imtiazepu@programmer.net>
2019-09-06 00:04:35 +0600

    TDD: basic validation

--- database/migrations/2019_09_05_083322_create_contacts_table.php
+++ database/migrations/2019_09_05_083322_create_contacts_table.php
@@ -16,6 +16,10 @@ class CreateContactsTable extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('birthday');
+            $table->string('company');
             $table->timestamps();
         });
     }
--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -3,20 +3,46 @@
 namespace Tests\Feature;
 
 use App\Contact;
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ContactsTest extends TestCase
 {
     use RefreshDatabase;
+
     /** @test */
     public function a_contact_a_can_be_added()
     {
-        $this->withoutExceptionHandling();
+        $this->post('/api/contacts', $this->data());
+        $contact = Contact::first();
 
-        $this->post('/api/contacts', ['name' => 'Test Name']);
-
-        $this->assertCount(1, Contact::all());
+        $this->assertEquals('Test Name', $contact->name);
+        $this->assertEquals('01616567207', $contact->phone);
+        $this->assertEquals('test@email.com', $contact->email);
+        $this->assertEquals('22/03/1987', $contact->birthday);
+        $this->assertEquals('ABC String', $contact->company);
     }
+
+    /** @test */
+        public function fields_are_required(){
+            collect(['name', 'phone', 'email', 'birthday', 'company'])
+                ->each(function ($field){
+                $response = $this->post('/api/contacts',
+                    array_merge($this->data(),[$field=>'']));
+
+                $response->assertSessionHasErrors($field);
+                $this->assertCount(0, Contact::all());
+            });
+        }
+
+        private function data()
+        {
+            return [
+                'name' => 'Test Name',
+                'phone' => '01616567207',
+                'email' => 'test@email.com',
+                'birthday' => '22/03/1987',
+                'company' => 'ABC String'
+            ];
+        }
 }
